6088c30066ca5b8aa7185d76509d71d034b0fb6f
deoladed <leo.robert.mk@gmail.com>
2018-12-05 10:45:42 +0100

    exercice termine, papyrus ne fonctionne pas

---
+++ scripts/main.js
@@ -0,0 +1,48 @@
+/*function miseEnAttente()
+{
+ //Traitement
+ setTimeOut(fonctionAExecuter, 1000); //On attend 10secondes avant d'exécuter la fonction
+}
+function fonctionAExecuter()
+{
+alert('Hey le site xxxvidsxxx est trop bien. Viens dessus stp please')
+}*/
+
+
+
+
+ /*setTimeout(function() {alert('Hey le site xxxvidsxxx est trop bien. Viens dessus stp please')}, 2000);*/
+
+
+
+
+
+/*function ConfirmMessage() {
+       if (confirm("Voulez-vous changer la couleur de fond de page ?")) {
+           // Clic sur OK
+            echo '<meta http-equiv="refresh" content="5; url=google.com" />';
+       }
+   }
+
+   ConfirmMessage();*/
+
+
+/*function myFunction() {
+    confirm("Press a button!");
+}*/
+
+
+
+setTimeout(function() {
+	  if (confirm("Hey le site xxxvidsxxx est trop bien. Viens dessus stp please. Tu veux?")) {
+window.location.replace("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+  }
+}, 10000);
+
+
+function psyche(){
+$('body').css('background', 'magenta');
+$('p').css('color','blue').css('font-family', 'Courier New');
+$(':header').css('color', 'green').css('font-family', 'Comic Sans Ms');
+}
+
